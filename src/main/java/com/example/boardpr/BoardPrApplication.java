9772e712e185780b2be0249ef825575be8268671
yuuzinn <s110528@nate.com>
2024-03-11 17:18:36 +0900

    First Commit Project

---
+++ src/main/java/com/example/boardpr/BoardPrApplication.java
@@ -0,0 +1,13 @@
+package com.example.boardpr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardPrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoardPrApplication.class, args);
+    }
+
+}
