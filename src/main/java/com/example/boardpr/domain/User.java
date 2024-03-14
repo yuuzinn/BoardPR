f00f146287dc30b95dda6e3851e9d30ce1681ff1
yuuzinn <s110528@nate.com>
2024-03-14 17:58:33 +0900

    feat : 회원 가입 기능 추가

--- src/main/java/com/example/boardpr/domain/User.java
+++ src/main/java/com/example/boardpr/domain/User.java
@@ -1,15 +1,24 @@
 package com.example.boardpr.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(unique = true)
+    private String username;
 
+    private String password;
+
+    @Column(unique = true)
+    private String email;
 }
